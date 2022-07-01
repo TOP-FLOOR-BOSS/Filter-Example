7e927c4e11feb0fe4efa78eb61297f027fc2d54b
TOP-FLOOR-BOSS <engelliam71@gmail.com>
2022-07-01 12:04:08 +0200

    first commit

--- script.js
+++ script.js
@@ -1,3 +1,108 @@
+localStorage.setItem( 'property' , JSON.stringify(properties = [{
+    id: 1,
+    Suite: 'House',
+    location: 'Italy',
+    URL1: "/imgs/Italy Aprtment.jpg",
+    bedrooms: 5, 
+    price: 900000
+  },
+{
+  id: 2,
+  Suite: 'House',
+  location: 'Spain',
+  URL2: "/imgs/Spain House.webp",
+  bedrooms: 6, 
+  price: 1000000
+
+},
+{
+  id: 3,
+  Suite: 'House',
+  location: 'Brazil',
+  URL3: "/imgs/Brazil House.webp",
+  bedrooms: 8, 
+  price: 1000000
+
+},
+{
+  id: 4,
+  Suite: 'Apartment',
+  location: 'Strandfontein',
+  URL4: "/imgs/Superior-apartments-Waterfront.webp",
+  bedrooms: 7, 
+  price: 1000000
+
+},
+{
+  id: 5,
+  Suite: 'Apartment',
+  location: 'Portugal',
+  URL5: "/imgs/Apartm`ent in Portugal.jpeg",
+  bedrooms: 4, 
+  price: 1000000
+},
+{
+  id: 6,
+  Suite: 'House',
+  location: 'Greece',
+  URL6: "/imgs/Greece house.webp",
+  bedrooms: 5, 
+  price: 1000000
+},
+{
+  id: 7,
+  Suite: 'Apartment',
+  location: 'Dubai',
+  URL7: "/imgs/Dubai Apartment.jpg",
+  bedrooms: 3, 
+  price: 1000000
+},
+{
+  id: 8,
+  Suite: 'House',
+  location: 'Australia',
+  URL8: "/imgs/Australia House.webp",
+  bedrooms: 4, 
+  price: 1000000
+},
+{
+  id: 9,
+  Suite: 'House',
+  location: 'Germany',
+  URL9: "/imgs/Germany House.jpg",
+  bedrooms: 7, 
+  price: 1000000
+},
+{
+  id: 10,
+  Suite: 'House',
+  location: 'Paris',
+  URL10: "/imgs/France2 House.jpg",
+  bedrooms: 7, 
+  price: 1000000
+}
+]))
+
+
+//Search Function 
+function theFunction() {
+    var input, filter, ul, li, a, p;
+    input = document.getElementById("mySearch");
+    filter = input.value.toUpperCase();
+    ul = document.getElementById("myMenu");
+    li = ul.getElementsByTagName("li");
+    for (p = 0; p < li.length; p++) {
+      a = li[p].getElementsByTagName("a")[0];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        li[p].style.display = "";
+      } else {
+        li[p].style.display = "none";
+      }
+    }
+  }
+
+
+
 const btns = document.querySelectorAll('.btn');
 const storeProducts = document.querySelectorAll('.store-product');
 // const search = document.getElementById(search);
